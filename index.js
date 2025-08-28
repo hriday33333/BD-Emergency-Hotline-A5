d64dc01b1956e6179645f45ae670a7c863def71f
hriday333 <zakirhasanhriday@gmaik.com>
2025-08-28 22:07:31 +0600

    added hert icon funtionality

---
+++ index.js
@@ -0,0 +1,13 @@
+let count = 0;
+  const cards = document.querySelectorAll(".card");
+  const counter = document.getElementById("count");
+
+  for (let card of cards) {
+    card.onclick = function () {
+      count++;
+      counter.innerText = count;
+      if (card.classList.contains("fa-heart")) {
+        card.style.color = "red";
+      }
+    };
+  }
